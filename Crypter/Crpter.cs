49f05d9d53520c3739487b3c9ef647bdef28f01e
Sneha Singh <45618796+snehasi2703@users.noreply.github.com>
2019-06-03 14:11:51 -0400

    Initial commit

---
+++ Crypter/Crpter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Crypter
+{
+   
+
+
+}
---
+++ WebApplication4/Controllers/MessageController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WebApplication4.Controllers
+{
+    public class MessageController : ApiController
+    {
+        public static IDictionary<string, string> hashDictionary = new Dictionary<string, string>();
+        [System.Web.Http.HttpPost]
+        public string Post(string id)
+        {
+            var hashValue = CrypterHelper.Encrypt(id);
+            if(!hashDictionary.ContainsKey(hashValue))
+                hashDictionary.Add(hashValue, id);
+            return hashValue;
+
+        }
+        public string Get(string id)
+        {
+            if(hashDictionary.ContainsKey(id))
+                return hashDictionary[id];
+            else
+                return "Message not found";  
+        }
+    }
+}
